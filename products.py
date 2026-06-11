ffd49804d3cbcd62e9b62ec128d32b9e8b78e3d2
Juan Magno Charney <70646915+30Charney@users.noreply.github.com>
2026-06-11 10:39:49 -0300

    Update products.py

--- products.py
+++ products.py
@@ -11,3 +11,14 @@ def add_product(name, quantity, price):
 
     conn.commit()
     conn.close()
+
+def list_products():
+    conn = sqlite3.connect("inventory.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM products")
+
+    for product in cursor.fetchall():
+        print(product)
+
+    conn.close()
